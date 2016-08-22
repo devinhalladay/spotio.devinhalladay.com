2f61a67a86fde10f06d0d62072b5f1dabaacb668
Devin Halladay <devin@devinhalladay.com>
2016-08-21 20:28:20 -0400

    Added JS to fetch latest release name and inject downlaod url for the binary

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+$(document).ready(function () {
+  GetLatestReleaseInfo();
+});
+
+
+function GetLatestReleaseInfo() {
+  $.getJSON("https://api.github.com/repos/devinhalladay/spotio/tags").done(function (json) {
+    var release = json[0];
+    var name = release.name;
+    var downloadURL = release.zipball_url;
+    $("#spotio-download-button").attr("href", "https://github.com/devinhalladay/spotio/releases/download/" + name + "/Spotify.zip");
+  });
+}
